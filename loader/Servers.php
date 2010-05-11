1599cbc20de108e258d419a1e148907caf63659c
barce <jbarcelona@gmail.com>
2010-05-11 19:10:48 -0400

    adding Servers.php

---
+++ loader/Servers.php
@@ -0,0 +1,37 @@
+<?php
+
+
+require_once(BASEPATH.'lib/spyc.php');
+echo 'loaded spyc';
+
+class Servers {
+
+  public static $a_connections = array();
+
+  function __construct() {
+    // empty constructor
+  }
+
+  static getConnections() {
+    self::$a_connections = Spyc::YAMLLoad(BASEPATH.'connect.yaml');
+
+  }
+
+}
+
+function dbConnect() {
+
+  $a_db = Servers::getConnections();
+  $dbh  = mysql_connect($a_db['a_dbConnect']['host'], $a_db['a_dbConnect']['user'], $a_db['a_dbConnect']['pass'], $bool_new_link);
+
+  if ( !$dbh || !mysql_select_db($a_db['a_dbConnect']['db'])) {
+    header("location: /sorry_db_down.php");^M
+    dieGracefully('dbConnect failed :: ' .$_SERVER['PHP_SELF'].' :: mysql_error()= ' .mysql_error());
+    return false;  //we catch this problem when func called
+  } else  {
+    return $dbh;
+  }
+
+}
+
+?>
